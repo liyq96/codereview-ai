7cfd75d741dcebecb33f690fde2cc28f363379fa
liyq96 <64461466+liyq96@users.noreply.github.com>
2025-03-20 20:58:12 +0800

    feat:优化

--- internal/service/code_review.go
+++ internal/service/code_review.go
@@ -27,7 +27,7 @@ func (s *CodeReviewService) SubmitCodeReview(code string) {
 	prompt, err := ReadMarkdownFile(s.cfg.Ai.PromptFilePath)
 	if err != nil {
 		fmt.Printf("ReadMarkdownFile error: %v\n", err)
-		prompt = "请帮我审查一下这段代码，依照markdown的格式回复建议，按照下面的分类分别给出改进意见。\n\n # 必要的改进内容 \n\n 可以选择优化的内容 \n\n 风格建议"
+		prompt = "请帮我审查一下这段代码，依照markdown的格式回复建议，按照下面的分类分别给出改进意见及原因，\n\n # 必需要改进内容 ## 原因 \n\n 建议优化的内容 ## 原因 \n\n 规范建议"
 	}
 	fmt.Println("----- standard request -----")
 	resp, err := client.CreateChatCompletion(
